10718170ece4949b511be37bb15bfd3e358b6489
‘Andy’ <1755331362@qq.com>
2022-08-23 20:25:12 +0800

    modified:   src/components/utils/user.ts modified:   src/interfaces/MainInterface.vue

--- src/components/utils/user.ts
+++ src/components/utils/user.ts
@@ -11,6 +11,7 @@ export const questionBankStorage = {
   content:JSON.parse(localStorage.getItem("questionBanks") ?? JSON.stringify([])), // questionBank's are saved locally in browser
   save:(storage_in?:questionBank)=>localStorage.setItem("questionBanks", storage_in ?? questionBankStorage.content),
 }
+
 function exportData(content:any, fileName:string, contentType:any) {
   var a = document.createElement("a");
   var file = new Blob([content], {type: contentType});
