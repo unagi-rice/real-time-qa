e93662452b588fab876b411bcef76bde28c867bb
Fusaaaann <fusen.5065@gmail.com>
2022-08-23 21:48:18 +0800

    remove excessive files

--- playground/register.ts
+++ playground/register.ts
@@ -1,10 +1,10 @@
 import { register, apps } from "@netless/fastboard";
 import App from "../src/index";
-import ExampleCounter from "../src/index2";// TODO: delete this line when submitting
+import ExampleCounter from "../src/index2";// HACK: delete this line when submitting
 import logo from "../src/logo.svg";
 
 export const registering = register({ kind: App.kind, src: App });
-export const registering2 = register({ kind: ExampleCounter.kind, src: ExampleCounter });
+export const registering2 = register({ kind: ExampleCounter.kind, src: ExampleCounter });//HACK:delete
 apps.clear();
 apps.push({
   kind: App.kind,
@@ -14,7 +14,7 @@ apps.push({
     fastboard.manager.addApp({ kind: App.kind });
   },
 });
-apps.push({
+apps.push({ // HACK: delete
   kind: ExampleCounter.kind,
   label: ExampleCounter.kind.replace(/([a-z])([A-Z])/g, "$1 $2"),
   icon: logo,
